6ab94461044e407b00847cc359cd6c2a2c3ed47f
M. Adjie Bagaskoro <30365444+Mabk96@users.noreply.github.com>
2022-01-06 16:50:47 +0700

    Task No 9. GET YOUR FREE QUOTE Grow With Us Now (Take Out)
    Done

--- app/Views/base/beranda/home.php
+++ app/Views/base/beranda/home.php
@@ -87,7 +87,7 @@ https://templatemo.com/tm-568-digimedia
               <li class="scroll-to-section"><a href="#services">Fitur</a></li>
               <li class="scroll-to-section"><a href="#portfolio">Tema</a></li>
               <li class="scroll-to-section"><a href="#blog">Harga</a></li>
-              <li class="scroll-to-section"><a href="#contact">Testimoni</a></li>
+              <li class="scroll-to-section"><a href="#">Testimoni</a></li>
               <li class="scroll-to-section"><a href="#contact">Kontak</a></li>
               <li class="scroll-to-section">
                 <div class="border-first-button"><a href="<?= base_url('login') ?>">Login</a></div>
@@ -355,43 +355,6 @@ https://templatemo.com/tm-568-digimedia
       </div>
     </div>
   </div>
-
-
-  <div id="free-quote" class="free-quote">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4 offset-lg-4">
-          <div class="section-heading  wow fadeIn" data-wow-duration="1s" data-wow-delay="0.3s">
-            <h6>Get Your Free Quote</h6>
-            <h4>Grow With Us Now</h4>
-            <div class="line-dec"></div>
-          </div>
-        </div>
-        <div class="col-lg-8 offset-lg-2  wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
-          <form id="search" action="#" method="GET">
-            <div class="row">
-              <div class="col-lg-4 col-sm-4">
-                <fieldset>
-                  <input type="web" name="web" class="website" placeholder="Your website URL..." autocomplete="on" required>
-                </fieldset>
-              </div>
-              <div class="col-lg-4 col-sm-4">
-                <fieldset>
-                  <input type="address" name="address" class="email" placeholder="Email Address..." autocomplete="on" required>
-                </fieldset>
-              </div>
-              <div class="col-lg-4 col-sm-4">
-                <fieldset>
-                  <button type="submit" class="main-button">Get Quote Now</button>
-                </fieldset>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-
 
   <div id="portfolio" class="our-portfolio section">
     <div class="services">
